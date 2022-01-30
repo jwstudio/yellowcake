d251fadf80a5d8703f2bf2ffd52ff3ae93a1598e
jameswhitby <46241335+jwstudio@users.noreply.github.com>
2022-01-30 00:43:41 +0000

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -109,8 +109,8 @@ export class Navigation extends Component {
                 </div>
               </span>
             </div>
-            <NavLink to="/default/">Default</NavLink>
-            <NavLink to="/contact/">Contact</NavLink>
+            <NavLink to="https://jww.netlify.app/#projects">Projects</NavLink>
+            <NavLink to="https://jww.netlify.app/#contact">Contact</NavLink>
           </div>
           <button
             className="Button-blank Nav--MenuButton"
